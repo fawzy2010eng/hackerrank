ef1c85d40e866ddcae12883cbf6b260a5c065f01
ahmed fawzy <42780497+fawzy2010eng@users.noreply.github.com>
2020-02-16 12:40:34 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+This is an introductory challenge to help you get familiar with the HackerRank coding environment.
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+5
+
+
+function solveMeFirst(a,b){
+    return a+b
+}
+console.log(solveMeFirst(2,3))
+
+
+
+
+
+
+
